3735adb365380e012d6539f09b100c2227610d9e
Jinshidjinu <jinshidjinu339@gmail.com>
2024-05-17 19:45:51 +0530

    users  hompage showing datas completed

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,48 +1,42 @@
-import React from 'react';
-import img from '../assets/images/FeaturedImg/johnwick.jpg';
-import Avesha from '../assets/images/FeaturedImg/Avesham.jpg';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
 import { BsStarFill, BsStarHalf } from 'react-icons/bs';
+import { Movie } from '@mui/icons-material';
 
-const  MovieCard = () => {
+const MovieCard = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const moviesData = JSON.parse(localStorage.getItem('movies')) || [];
+    setMovies(moviesData);
+  }, []);
+
   return (
-    <div className="flex flex-col  bg-black">
-      <h1 className="text-3xl p-4 text-start text-yellow-500">Actions</h1>
-      <div className="flex flex-wrap  gap-6">
-        <div className="flex flex-col items-center w-[280px] h-[500px]  p-5 bg-black rounded-lg transition-all duration-150 hover:scale-110">
-          <img src={img} alt="" className="rounded-lg w-full h-[400px] object-cover" />
-          <div className="flex flex-row justify-center items-center mt-5 ">
-            <h1 className="text-white">hello</h1>
-            <div className="flex">
-              <BsStarFill className="text-yellow-500" />
-              <BsStarFill className="text-yellow-500" />
-              <BsStarFill className="text-yellow-500" />
-              <BsStarFill className="text-yellow-500" />
-              <BsStarHalf className="text-yellow-500" />
+    <div className="flex flex-col bg-black">
+      <h1 className="text-3xl p-4 text-start text-yellow-500">Movies</h1>
+      <div className="flex flex-wrap gap-6 ">
+        {movies.map((movie) => (
+          <div
+            key={movie.name}
+            className="flex flex-col items-center w-[280px] h-[500px] p-5 bg-black rounded-lg transition-all duration-150 hover:scale-110"
+          >
+            <Link to="/moviesdetails">
+              <img
+                src={movie.image}
+                alt=""
+                className="rounded-lg w-full h-[400px] object-cover"
+              />
+            </Link>
+            <div className="flex flex-col justify-center items-center mt-5">
+              <h1 className="text-white font-bold font-serif">{movie.name}</h1>
+            
+              <h1 className="text-blue-400 text-xs">{movie.language}</h1>
             </div>
-            <h1>1999</h1>
           </div>
-        </div>
-          
-        <div className="flex flex-col items-center w-[280px] h-[500px]  p-5 bg-black rounded-lg transition-all duration-150 hover:scale-110">
-         <Link to='/moviesdetails'> <img src={Avesha} alt="" className="rounded-lg w-full h-[400px] object-cover" /></Link>
-          <div className="flex flex-row justify-center items-center mt-5 ">
-            <h1 className="text-white">hello</h1>
-            <div className="flex">
-              <BsStarFill className="text-yellow-500" />
-              <BsStarFill className="text-yellow-500" />
-              <BsStarFill className="text-yellow-500" />
-              <BsStarFill className="text-yellow-500" />
-              <BsStarHalf className="text-yellow-500" />
-            </div>
-            <h1 className='text-white'>1999</h1>
-          </div>
-        </div>
-        {/* Add more cards here */}
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default MovieCard;
--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,56 +1,75 @@
-import Navbar from "./Navbar"
-import IMg from '../assets/images/FeaturedImg/johnwick.jpg'
-import Aveshm from '../assets/images/FeaturedImg/Avesham.jpg'
+import Navbar from "./Navbar";
+import { useEffect, useState } from "react";
 import { FaCirclePlay } from "react-icons/fa6";
 import { BsStarFill, BsStarHalf } from 'react-icons/bs';
 import { CiStar } from "react-icons/ci";
 
 const MovieDetails = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const moviesData = JSON.parse(localStorage.getItem('movies')) || [];
+    setMovies(moviesData);
+  }, []);
+
   return (
     <div className="bg-black min-h-screen">
       <Navbar />
       <div className="bg-black w-full h-[1px]"></div>
-      <div className="w-full p-10 flex flex-col md:flex-row justify-evenly items-center mt-[120px] ">
-        <div className="w-full md:w-[600px] h-[600px] mb-8 md:mb-0">
-          <img className="bg-cover w-full h-[600px] rounded-2xl" src={Aveshm} alt="" />
-        </div>
-        <div className="bg-black w-full md:w-[500px] h-auto text-white flex flex-col gap-4 p-9 rounded">
-          <h1 className="font-bold">Aavesham</h1>
-          <h2 className="text-gray-400">Malayalam,English,Hindi,Kannada,Tamil</h2>
-          <p>Three teenagers reach Bangalore for their engineering degree and gets involved in a fight with seniors. They find a local gangster named Ranga to help them take revenge.</p>
-          <div className="flex">
-            <BsStarFill className="text-yellow-500" />
-            <BsStarFill className="text-yellow-500" />
-            <BsStarFill className="text-yellow-500" />
-            <BsStarFill className="text-yellow-500" />
-            <BsStarHalf className="text-yellow-500" />
-          </div>
-          <button className="bg-red-700 rounded-2xl w-[140px] h-[40px] flex gap-2 justify-center items-center">
-            <FaCirclePlay className="text-md" />
-            Watch Movie
-          </button>
-          <div className="border border-solid border-gray-800 w-[300px] flex flex-col gap-5 p-2">
-            <h1>Rate and Review</h1>
-            <form action="" className="flex flex-col gap-2">
-              <div className="flex">
-                <CiStar className="text-gray-300" />
-                <CiStar className="text-gray-300" />
-                <CiStar className="text-gray-300" />
-                <CiStar className="text-gray-300" />
-                <CiStar className="text-gray-300" />
+      <div className="w-full p-10 flex flex-col md:flex-row justify-evenly items-center mt-[120px]">
+        {movies.length > 0 ? (
+          movies.map((movie,) => (
+            <div key={movie.name} className="flex flex-col md:flex-row justify-evenly items-center">
+              <div className="w-full md:w-[600px] h-[600px] mb-8 md:mb-0">
+                <img
+                  className="bg-cover w-full h-[600px] rounded-2xl"
+                  src={movie.image}
+                  alt={movie.name}
+                />
               </div>
-              <textarea
-                name=""
-                id=""
-                className="bg-gray-600 w-[200px] text-white rounded-md resize-none outline-none "
-              ></textarea>
-              <button className="bg-blue-800 w-[100px] rounded-xl ">Submit</button>
-            </form>
-          </div>
-        </div>
+              <div className="bg-black w-full md:w-[500px] h-auto text-white flex flex-col gap-4 p-9 rounded">
+                <h1 className="font-bold">{movie.name}</h1>
+                <h2 className="text-gray-400">{movie.language}</h2>
+                <p>{movie.description}</p>
+                <div className="flex">
+                  <BsStarFill className="text-yellow-500" />
+                  <BsStarFill className="text-yellow-500" />
+                  <BsStarFill className="text-yellow-500" />
+                  <BsStarFill className="text-yellow-500" />
+                  <BsStarHalf className="text-yellow-500" />
+                </div>
+                <button className="bg-red-700 rounded-2xl w-[140px] h-[40px] flex gap-2 justify-center items-center">
+                  <FaCirclePlay className="text-md" /> Watch Movie
+                </button>
+                <div className="border border-solid border-gray-800 w-[300px] flex flex-col gap-5 p-2">
+                  <h1>Rate and Review</h1>
+                  <form action="" className="flex flex-col gap-2">
+                    <div className="flex">
+                      <CiStar className="text-gray-300" />
+                      <CiStar className="text-gray-300" />
+                      <CiStar className="text-gray-300" />
+                      <CiStar className="text-gray-300" />
+                      <CiStar className="text-gray-300" />
+                    </div>
+                    <textarea
+                      name=""
+                      id=""
+                      className="bg-gray-600 w-[200px] text-white rounded-md resize-none outline-none"
+                    ></textarea>
+                    <button className="bg-blue-800 w-[100px] rounded-xl">
+                      Submit
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p className="text-white">No movies found.</p>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MovieDetails
+export default MovieDetails;
